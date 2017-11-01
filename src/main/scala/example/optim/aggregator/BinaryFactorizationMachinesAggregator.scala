8cb0f7759fb7945729d9fcc2788df2c25c6c945b
Shintaro Murakami <mrkm4ntr@gmail.com>
2017-11-01 14:04:52 +0900

    Implement factorization machines

---
+++ src/main/scala/example/optim/aggregator/BinaryFactorizationMachinesAggregator.scala
@@ -0,0 +1,57 @@
+package example.optim.aggregator
+
+import example.feature.Point
+import org.apache.spark.broadcast.Broadcast
+import org.apache.spark.ml.classification.BinaryFactorizationMachinesModel
+import org.apache.spark.ml.linalg.{Matrices, Vector, Vectors}
+
+class BinaryFactorizationMachinesAggregator(k: Int, n: Int)(bcCoefficients: Broadcast[Vector])
+  extends DifferentiableLossAggregator[Point, BinaryFactorizationMachinesAggregator] {
+
+  override protected val dim: Int = bcCoefficients.value.size
+
+  @transient
+  private lazy val coefficientsArray = bcCoefficients.value.toArray
+  @transient
+  private lazy val intercept = coefficientsArray.last
+  @transient
+  private lazy val (coefficients, v) = {
+    val (coefficients, v) = coefficientsArray.init.splitAt(n)
+    (Vectors.dense(coefficients), Matrices.dense(k, n, v))
+  }
+
+  private def binaryUpdateInPlace(features: Vector, label: Double): Unit = {
+    val localGradientArray = gradientSumArray
+    val margin = -BinaryFactorizationMachinesModel.predict(features, coefficients, intercept, v)
+    val multiplier = 1.0 / (1.0 + math.exp(margin)) - label
+
+    val precomputed = v.multiply(features) // f * 1
+    features.foreachActive { case (i, value) =>
+        localGradientArray(i) += multiplier * value
+        precomputed.foreachActive { case (f, pre) =>
+            localGradientArray(f) += multiplier * value * (pre - v(f, i) * value)
+        }
+    }
+    // Intercept
+    localGradientArray(dim - 1) += multiplier
+
+    def log1pExp(x: Double) = if (x > 0) {
+      x + math.log1p(math.exp(-x))
+    } else {
+      math.log1p(math.exp(x))
+    }
+
+    if (label > 0) {
+      lossSum += log1pExp(margin)
+    } else {
+      lossSum += log1pExp(margin) - margin
+    }
+  }
+
+  override def add(point: Point): BinaryFactorizationMachinesAggregator = point match {
+    case Point(label, features) =>
+      binaryUpdateInPlace(features, label)
+      weightSum += 1.0
+      this
+  }
+}
---
+++ src/main/scala/org/apache/spark/ml/classification/BinaryFactorizationMachines.scala
@@ -0,0 +1,120 @@
+package org.apache.spark.ml.classification
+
+import breeze.optimize.{CachedDiffFunction, LBFGS}
+import breeze.linalg.{DenseVector => BDV}
+import example.feature.Point
+import example.optim.aggregator.BinaryFactorizationMachinesAggregator
+import example.optim.loss.RDDLossFunction
+import org.apache.spark.ml.linalg.{BLAS, DenseVector, Matrices, Matrix, Vector, Vectors}
+import org.apache.spark.ml.param.{IntParam, ParamMap, Params}
+import org.apache.spark.ml.param.shared._
+import org.apache.spark.ml.util.Identifiable
+import org.apache.spark.sql.{Dataset, Row}
+
+import scala.collection.mutable
+
+
+trait HasK extends Params {
+
+  final val k: IntParam = new IntParam(this, "k", "")
+
+  final def getK: Int = $(k)
+}
+
+trait BinaryFactorizationMachinesParams extends ProbabilisticClassifierParams
+  with HasMaxIter with HasTol with HasAggregationDepth with HasThreshold
+  with HasK {
+
+  def setMaxIter(value: Int): this.type = set(maxIter, value)
+  setDefault(maxIter -> 10)
+
+  def setTol(value: Double): this.type = set(tol, value)
+  setDefault(tol -> 1E-6)
+
+  def setAggregationDepth(value: Int): this.type = set(aggregationDepth, value)
+  setDefault(aggregationDepth -> 2)
+
+  def setThreshold(value: Double): this.type = set(threshold, value)
+  setDefault(threshold -> 0.5)
+
+  def setK(value: Int): this.type = set(k, value)
+  setDefault(k -> 4)
+}
+
+class BinaryFactorizationMachines(override val uid: String)
+  extends ProbabilisticClassifier[Vector, BinaryFactorizationMachines, BinaryFactorizationMachinesModel]
+  with BinaryFactorizationMachinesParams {
+
+  def this() = this(Identifiable.randomUID("binfm"))
+
+  override def copy(extra: ParamMap): BinaryFactorizationMachines = defaultCopy(extra)
+
+  override protected def train(dataset: Dataset[_]): BinaryFactorizationMachinesModel = {
+    val points = dataset.select($(labelCol), $(featuresCol)).rdd.map {
+      case Row(label: Double, features: Vector) => Point(label, features)
+    }
+
+    val numOfFeatures = points.first().features.size
+    val numOfCoefficients = numOfFeatures + 1 + $(k) * numOfFeatures
+
+    val optimizer = new LBFGS[BDV[Double]]($(maxIter), 10, $(tol))
+    val costFun = new RDDLossFunction(points, new BinaryFactorizationMachinesAggregator($(k), numOfFeatures)(_), 0.0, $(aggregationDepth))
+    val init = Vectors.zeros(numOfCoefficients)
+    val states = optimizer.iterations(new CachedDiffFunction(costFun), new BDV(init.toArray))
+    val arrayBuilder = mutable.ArrayBuilder.make[Double]
+    var state: optimizer.State = null
+    while (states.hasNext) {
+      state = states.next()
+      arrayBuilder += state.adjustedValue
+    }
+    val allCoefficients = state.x.toArray.clone
+    val intercept = allCoefficients.last
+    val (coefficients, v) = {
+      val (coefficients, v) = allCoefficients.init.splitAt(numOfFeatures)
+      (Vectors.dense(coefficients), Matrices.dense($(k), numOfFeatures, v))
+    }
+    new BinaryFactorizationMachinesModel(uid, coefficients, intercept, v)
+  }
+}
+
+class BinaryFactorizationMachinesModel(
+  override val uid: String,
+  val coefficients: Vector,
+  val intercept: Double,
+  val v: Matrix // f * n
+) extends ProbabilisticClassificationModel[Vector, BinaryFactorizationMachinesModel]
+  with BinaryFactorizationMachinesParams {
+
+  override protected def raw2probabilityInPlace(rawPrediction: Vector): Vector =
+    Vectors.dense(rawPrediction.asInstanceOf[DenseVector].values.map(p => 1.0 / (1.0 + math.exp(-p))))
+
+  override def numClasses: Int = 2
+
+  override protected def predictRaw(features: Vector): Vector =
+    Vectors.dense(Array(BinaryFactorizationMachinesModel.predict(features, coefficients, intercept, v)))
+
+  override def copy(extra: ParamMap): BinaryFactorizationMachinesModel = defaultCopy(extra)
+
+  override protected def probability2prediction(probability: Vector): Double =
+    if (probability(0) > $(threshold)) 1 else 0
+
+  protected override def raw2prediction(rawPrediction: Vector): Double =
+    probability2prediction(raw2probability(rawPrediction))
+}
+
+object BinaryFactorizationMachinesModel {
+
+  def predict(features: Vector, coefficients: Vector, intercept: Double, v: Matrix) = {
+    val interactive = v.rowIter.foldLeft(0.0) { case (acc, v) =>
+      var fst = 0.0
+      var snd = 0.0
+      features.foreachActive { case (index, x) =>
+        val vx = v(index) * x
+        fst += vx
+        snd += math.pow(vx, 2.0)
+      }
+      acc + math.pow(fst, 2.0) - snd
+    }
+    BLAS.dot(features, coefficients) + intercept + 0.5 * interactive
+  }
+}
